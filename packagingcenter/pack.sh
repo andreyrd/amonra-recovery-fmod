e780e86995ed04ca8a77a50f08e60a837c4ad641
andreyrd1@gmail.com <andreyrd1@gmail.com@426b2cf1-53c6-7b77-0569-931355fa8e63>
2012-12-21 00:30:38 +0000

    got rid of the packagingcenter folder. it causes lots of problems with svn since the files change so much. stupid svn
    git-svn-id: https://amonra-recovery-fmod.googlecode.com/svn/trunk@7 426b2cf1-53c6-7b77-0569-931355fa8e63

--- packagingcenter/pack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp ~/Donut/out/target/product/dream-open/system/bin/recovery old-recovery.img-ramdisk/sbin/recovery
-./repack-bootimg.pl old-recovery.img-kernel.gz old-recovery.img-ramdisk recovery.img
-
